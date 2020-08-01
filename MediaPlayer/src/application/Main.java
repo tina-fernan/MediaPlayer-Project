66654de417aab8a7164c415d2c263ad854b9b09e
tinas <tinas@Plato>
2020-08-01 08:46:43 +0200

    class Player extends BorderPane

--- MediaPlayer/src/application/Main.java
+++ MediaPlayer/src/application/Main.java
@@ -1,6 +1,9 @@
 package application;
 	
+import java.io.File;
+
 import javafx.application.Application;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Menu;
@@ -13,12 +16,15 @@ public class Main extends Application {
 	MenuBar menu;
 	Menu fileMenu;
 	MenuItem openItem;
+	FileChooser mediaChooser;
 	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
+			
+			mediaChooser=new FileChooser();
 			
 			menu=new MenuBar();
 			fileMenu=new Menu("File");
@@ -28,7 +34,8 @@ public class Main extends Application {
 			menu.getMenus().add(fileMenu);
 			
 			openItem.setOnAction((e) -> {
-				System.out.println("clicked on open!");
+				File mediaFile=mediaChooser.showOpenDialog(primaryStage);
+				System.out.println(mediaFile.getAbsolutePath());
 			});
 			
 			root.setTop(menu);
